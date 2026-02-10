12f913c70605e56be4427dc9c9ce2d2e3da0fb23
Aaaaa0850 <oshama0850@gmail.com>
2026-02-11 03:30:09 +0900

    tailwindを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import node from '@astrojs/node';
 
 import vue from '@astrojs/vue';
 
+import tailwindcss from "@tailwindcss/vite"; 
+
 //import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-
-  integrations: [vue()]
+  integrations: [vue()],
+  vite: { 
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
